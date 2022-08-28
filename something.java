b34f4eda7718437f4c2be336906e80bb11057440
dg311 <83474866+dg311@users.noreply.github.com>
2022-08-28 12:16:17 -0400

    Create something.java

---
+++ something.java
@@ -0,0 +1,5 @@
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Omg my first java project which is very basic and I only know how to write text! Yay!!!");
+	}
+}
